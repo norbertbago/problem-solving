ba074f38882c2ee1aed8a4433b82493aaa42b150
Norbert Bago <norbert.bago@gmail.com>
2021-06-09 21:48:27 +0200

    update programming tic tac toe game

--- game_tic_tac_toe/main.py
+++ game_tic_tac_toe/main.py
@@ -1,18 +1,20 @@
-from welcome_window import welcome_window
+from welcome_window import welcome_window, start_game
 from game_board import default_game, print_game
-
 
 rows = 3
 colums = 3 
-
-player_1 = "x"
-player_2 = "o"
-
-
-welcome_window()
 game = default_game(rows, colums)
-print_game(game, rows, colums)
+empty_places = rows*colums
 
-# input(str("Prajete si spusit hru(y/n)?"))
+#welcome_window()
+for round_id in range(1,6):
+    print("Round number is ", round_id)
+    for player in range(0, 2):
+        print_game(game, rows, colums)
+        
+
+
+
+
 
 
